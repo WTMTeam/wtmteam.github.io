81b41901144ec9df8022a07536b80151ff87808a
Sam <60146956+srudqvist@users.noreply.github.com>
2023-05-22 09:53:43 -0500

    testing

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
 	reactStrictMode: true,
 	// swcMinify: true,
+	output: "export",
+	basePath: "/gh-pages",
 	images: {
 		unoptimized: true,
 		domains: ["user-images.githubusercontent.com"],
