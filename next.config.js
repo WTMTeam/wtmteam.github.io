2597d3d9adc92edff0a92c963d68530cc450fff5
Sam <60146956+srudqvist@users.noreply.github.com>
2023-05-22 08:55:29 -0500

    config changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
 	reactStrictMode: true,
 	// swcMinify: true,
 	images: {
 		domains: ["user-images.githubusercontent.com"],
+		unoptimized: true,
 	},
+	assetPrefix: isProd ? "wtmteam.github.io" : "",
 };
 
 module.exports = nextConfig;
